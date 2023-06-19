33b16fa9502e27d73190e09f25f6f77cf3ed35d6
Victor Carpes <victorcarpes@outlook.com>
2023-06-19 11:46:42 +0100

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame;
+
+public class Position {
+	//Atributes
+	private int row;
+	private int column;
+	
+	//Constructors
+	public Position() {
+		
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	//Getters & Setters
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
